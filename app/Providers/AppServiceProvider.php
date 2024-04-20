4638f2b500c736aa7b8d2f3ef2d038af0fc43982
br3ekachu <br3ekachu@gmail.com>
2024-04-21 00:29:46 +0300

    upd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/auth/reset-password?token=$token&email={$notifiable->getEmailForPasswordReset()}";
         });
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+            URL::forceRootUrl(config('app.url', env('APP_URL')));
+        }
     }
 }
