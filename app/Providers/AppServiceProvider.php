51d2dc8ec9085a41bb86e49255851e1ed62047bd
br3ekachu <br3ekachu@gmail.com>
2024-04-21 00:31:57 +0300

    upd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,6 @@ class AppServiceProvider extends ServiceProvider
             return config('app.frontend_url')."/auth/reset-password?token=$token&email={$notifiable->getEmailForPasswordReset()}";
         });
         if (env('APP_ENV') !== 'local') {
-            URL::forceScheme('https');
             URL::forceRootUrl(config('app.url', env('APP_URL')));
         }
     }
