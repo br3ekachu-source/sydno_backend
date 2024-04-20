214d3dd369d1258e493af0fc905da542ae9557b7
br3ekachu <br3ekachu@gmail.com>
2024-04-21 01:05:06 +0300

    upd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             '*'
         ]);
-
+        $middleware->trustProxies('*');
     })
     // ->withMiddleware(function (Middleware $middleware) {
     //     $middleware->trustProxies('*');
